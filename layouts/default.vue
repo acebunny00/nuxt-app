d8a0e70f7bf9581ca1c42918f2f239804e6e16d6
DrKim <acebunny00@gmail.com>
2023-12-30 07:59:03 +0900

    🔧 Use pages and layouts and Set from DevTools
    - Using Pages by DevTools Pages https://nuxt.com/docs/api/components/nuxt-page
    - Using Layout https://nuxt.com/docs/api/components/nuxt-layout
    - Add SEO in app.vue by DevTools Open Graph
    - Enable timeline by from DevTools Timeline

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav>
+		<li>
+			<NuxtLink to="/">Home</NuxtLink>
+		</li>
+		<li v-show="true">
+			<NuxtLink to="/index_dev">Home dev</NuxtLink>
+		</li>
+	</nav>
+	<slot />
+</template>
+
+<script lang="ts" setup></script>
+
+<style>
+	li {
+		display: inline;
+		margin-right: 20px;
+	}
+</style>
